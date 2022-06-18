649dbd1f49961f8cdfab6bea27bd7d006c1ea0b8
codersunil-28 <tsunil28k@gmail.com>
2022-06-19 01:37:59 +0800

    Topic 3: First Program in Swift

---
+++ first_program.swift
@@ -0,0 +1,22 @@
+
+// First program in Swift
+
+print ("Hello, World!")
+// Prints: Hello, world!
+
+print ("I am an iOS Dev")
+// Prints: I am an iOS Dev
+
+// Note:
+
+// Do not need to import a separate library for functionality like input/output or string handling.
+// Code written at global scope is used as the entry point for the program, so you don’t need a main() function.
+// Also don’t need to write semicolons at the end of every statement.
+
+
+
+
+
+
+
+// Command to run the program: swift file_name.swift
