551fb934fe7b2f93062e47770132618e41def879
codersunil-28 <tsunil28k@gmail.com>
2022-06-19 00:18:17 +0800

    Topic1: About Swift Programming

---
+++ aboutswift.swift
@@ -0,0 +1,39 @@
+// About Swift
+
+// Swift is a programming language used to write Software.
+
+// You can write software using Swift for phones, desktops, servers or anything else that runs code.
+
+// Swift is a safe, fast, and interactive programming language.
+
+/* It combines the best in modern language thinking with wisdom from the wider Apple engineering culture 
+and the diverse contributions from its open-source Community. */
+
+// The compiler is optimized for performance and the language is optimized for development, without compromising on either.
+
+// Swift is friendly to new Programmers.
+
+// It's an industrial-quality programming language that's as expressive and enjoyable as a scripting language.
+
+/* Swift defines away large classes of common programming errors by adopting modern programming patterns:
+
+- Variables are always initialized before use.
+- Array indices are checked for out-of-bounds errors.
+- Integers are checked for overflow.
+- Optionals ensure that nil values are handled explicitly.
+- Memory is managed automatically.
+- Error handling allows controlled recovery from uxpected failures.  
+
+*/
+
+// Swift code is compiled and optimised to get the most out of modern hardware.
+
+/* The syntax and standard library have been designed based on the guiding principle 
+that the obivous way to write your code should also perform the best. */
+
+/* Its combination of safety and speed make Swift an excellent choice for everything from
+"Hello, World!" to an entire operating system. */
+
+/* Swift combines powerful type inference and pattern matching with a modern, lightweight syntax, 
+allowing complex ideas to be expressed in a clear and concise manner. 
+As a result, code is not just easier to write, but easier to read and maintain as well. */
